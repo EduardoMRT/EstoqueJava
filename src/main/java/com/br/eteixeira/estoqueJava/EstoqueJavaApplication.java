7ed9032bd1561edd51109361c3978fbed2368ae6
EduardoMRT <eduksoficial@gmail.com>
2024-10-31 08:51:02 -0300

    Projeto Spring

---
+++ src/main/java/com/br/eteixeira/estoqueJava/EstoqueJavaApplication.java
@@ -0,0 +1,13 @@
+package com.br.eteixeira.estoqueJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoqueJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstoqueJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/eteixeira/estoqueJava/EstoqueJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.eteixeira.estoqueJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoqueJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
